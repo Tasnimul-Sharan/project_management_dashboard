7322bd11009fb59a3dfcc741fc4268c1e1beeb85
Tasnimul <tasnimulsharan8962@gmail.com>
2024-11-25 13:37:14 +0600

    redirrect solved

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,6 +2,8 @@ import { combineReducers } from "@reduxjs/toolkit";
 import { persistReducer} from "redux-persist";
 import storageEngine from "./forPersistErrorSolve";
 import { baseApi } from "./api/baseApi";
+import authSlice from "./slice/authSlice";
+
 
 
 const persistConfig = {
@@ -12,8 +14,8 @@ const persistConfig = {
 
 
 const reducer=combineReducers({
-    [baseApi.reducerPath]: baseApi.reducer
-   
+    [baseApi.reducerPath]: baseApi.reducer,
+    authUI:authSlice
 })
 
 export const persistedReducer = persistReducer(persistConfig, reducer)
